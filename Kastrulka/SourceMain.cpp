1b36953a3164f9146dfc3ae6690886d032052afe
Yulia <yuliaduma25@gmail.com>
2024-03-10 14:54:05 +0200

    start

---
+++ Kastrulka/SourceMain.cpp
@@ -0,0 +1,13 @@
+#include "Header1.h"#include "Header11.h"
+int main()
+{
+	ifstream file("text1.txt");
+	matrix m;
+	matrix* a = new matrix[100];
+	int counter = 0;
+	int** array2 = new int* [4];
+	for (int i = 0; i < 4; i++)
+	{
+		array2 = new int* [4];
+	}
+}
