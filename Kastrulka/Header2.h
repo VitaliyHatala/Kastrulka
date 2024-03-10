ec2a5f335661b0ef2699c0e7d19fbc8b3d6bb444
SpaceHunter19 <161298211+SpaceHunter19@users.noreply.github.com>
2024-03-10 22:11:25 +0200

    Update Header2.h

--- Kastrulka/Header2.h
+++ Kastrulka/Header2.h
@@ -1,10 +1,12 @@
 #pragma once
-#include "Header.h"
-
-class matrix2 : public matrix {
+#include "Header1.h"
+class matrix2 : public matrix 
+{
+private: 
+int element = 1;
 public:
-   matrix2() : matrix() {}
-   matrix2(int r, int c, int** arr) : matrix(r, c, arr) {}
-   matrix2(matrix& m);
-   void fillWithOnes();
+matrix2();
+matrix2(int r, int c, int** array); matrix2(matrix& m);
+
+ void PrintM();
 };
